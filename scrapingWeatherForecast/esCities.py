7ab4c0e53f0dc4d96220b49af7d57f207f91e7ff
NamizataS <nsangare@icloud.com>
2022-06-09 12:12:28 +0200

    cities in elastic search database

--- scrapingWeatherForecast/esCities.py
+++ scrapingWeatherForecast/esCities.py
@@ -1,6 +1,37 @@
 from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
+import pandas as pd
+
+
+def generate_data(docs):
+    for docu in docs:
+        yield {
+            "_index": "cities_index",
+            "_source": {k: v if v else None for k, v in docu.items()},
+        }
+
 
 if __name__ == "__main__":
     LOCAL = False
     es_client = Elasticsearch(hosts="http://localhost:9200/")
     print(es_client.ping())
+    es_client.options(ignore_status=[400, 401, 404]).indices.delete(index="cities_index")
+    mapping = {
+        "properties": {
+            "city": {
+                "type": "text"
+            },
+            "location": {
+                "type": "geo_point"
+            },
+            "country": {
+                "type": "keyword"
+            }
+        }
+    }
+    es_client.indices.create(index="cities_index", mappings=mapping)
+    df_cities = pd.read_csv("worldcities.csv")
+    df_cities['location'] = df_cities.lat.astype(str).str.cat(df_cities.lng.astype(str), sep=',')
+    df_cities_es = df_cities[['city', 'location', 'country']]
+    documents = df_cities_es.to_dict(orient="records")
+    bulk(es_client, generate_data(documents))
--- scrapingWeatherForecast/test.py
+++ scrapingWeatherForecast/test.py
@@ -1,10 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+from elasticsearch import Elasticsearch
 
 if __name__ == "__main__":
+    '''
     cookies = {"CONSENT": "YES+cb.20210720-07-p0.en+FX+410"}
     soup = BeautifulSoup(requests.get("https://www.google.com/search?q=weather+paris", cookies=cookies).content,
                          "html.parser")
     temp = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
     time_sky_desc = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
     print("{}\n{}".format(temp, time_sky_desc))
+    
+    df = pd.read_csv("worldcities.csv")
+    df['location'] = df.lat.astype(str).str.cat(df.lng.astype(str), sep=',')
+    documents = df.to_dict(orient="records")
+    print(documents[0:2])
+    new_df = df[['city', 'location', 'country']]
+    print(new_df.head())
+    '''
+    es_client = Elasticsearch(hosts="http://localhost:9200/")
+    print(es_client.ping())
+    print(es_client.indices.get(index="cities_index"))
+    query = {
+        "term": {
+            "city": {
+                "value": "paris"
+            }
+        }
+    }
+    print(es_client.search(index="cities_index", query=query))
